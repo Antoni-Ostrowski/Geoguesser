2a911e9c6d3b747421b3b0be47c8dfe83403f9ea
antost360 <antost360@gmail.com>
2023-05-01 02:12:05 +0200

    geogueeser 1.2

--- app.js
+++ app.js
@@ -1,112 +1,129 @@
 //! początkowe ustawianie mapy, okienka, źródło mapy(1 link)
 //deklaracja mapy i deklaracja wyskakujacego okienka
-var map = L.map("map", { minZoom: 3 }).setView([54.34343507696856, 18.87529659397512], 4)
-var popup = L.popup()
+var map = L.map("map", { minZoom: 3, zoomSnap: 0.25 }).setView([36.38309138759306, 22.3209691411363], 3),
+  popup = L.popup()
+
 //zrodlo mapy
 L.tileLayer("http://a.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png", {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map)
 
-//! dodanie do mapy krajow i kontrola co sie dzieje przy "eventach" myszki
-var geojson
-//co sie stanie po kliknieciu na kraj
-function zoomToFeature(e) {
-  if (wylosowany_kraj == e.target.feature.properties.NAME) {
-    console.log("zgadnieto!")
-    alert("zgadnieto!")
-    punkty_text++
-    pokaz_mape()
-  } else {
-    console.log("nie zgadnieto :/")
-    alert("nie zgadnieto :/")
+function wybor(wybor) {
+  wybor == "europa" ? pokaz_europa(europa, 44) : pokaz_europa(kraje_all, 195)
+}
 
-    if (serca_text.length == 1) {
-      serca_text = serca_text.slice(0, -1)
-      alert("przegrana")
-    }
-    serca_text = serca_text.slice(0, -1)
-
-    pokaz_mape()
-  }
-  // console.log(e.target.feature.properties.NAME)
-  map.setView([54.34343507696856, 18.87529659397512], 4)
-}
-//co sie stanie po wyjachaniu z kraju myszka
-function resetHighlight(e) {
-  geojson.resetStyle(e.target)
-}
-// co sie stanie po najechaniu na kraj
-function highlightFeature(e) {
-  var layer = e.target
-  //ustawia styl
-  layer.setStyle({
-    weight: 5,
-    color: "#666",
-    dashArray: "",
-    fillOpacity: 0.7,
-  })
-
-  layer.bringToFront()
-}
-//dopisanie funkcji do eventow
-function onEachFeature(feature, layer) {
-  layer.on({
-    mouseover: highlightFeature,
-    mouseout: resetHighlight,
-    click: zoomToFeature,
-  })
-}
-//dodawnie do mapy
-geojson = L.geoJson(kraje, {
-  onEachFeature: onEachFeature,
-}).addTo(map)
-
-//! losowanie liczb
-var aaa = []
-const liczba_kraji = 44
-for (var i = 0; i <= liczba_kraji; i++) {
-  aaa.push(i)
-}
-// console.log(aaa)
-function* shuffle(tablica) {
-  var i = tablica.length
-
-  while (i--) {
-    yield tablica.splice(Math.floor(Math.random() * (i + 1)), 1)[0]
-  }
-}
-var ranNums = shuffle(aaa)
-//test liczb losowaych
-// for (var i = 1; i <= 5; i++) {
-//   console.log(kraje.features[ranNums.next().value].properties.NAME)
-// }
-//! odświeżanie mapy, wczytywanie liczby serc, wszytywanie liczby punktow, pokazanie mapy ukrycie ekranu startowego
 var wylosowany_kraj
 var serca_text = "❤❤❤❤❤"
 var punkty_text = 0
-const main = document.getElementById("main")
-const pytanie = document.getElementById("pytanie")
+const main = document.getElementById("main"),
+  pytanie = document.getElementById("pytanie"),
+  err = document.getElementById("err"),
+  popup_1 = document.getElementById("popup_1_id")
 
-function pokaz_mape() {
+function pokaz_europa(zrodlo, wskazana_liczba_kraji) {
+  //! dodanie do mapy krajow i kontrola co sie dzieje przy "eventach" myszki
+  var geojson
+  //co sie stanie po kliknieciu na kraj
+  function zoomToFeature(e) {
+    if (wylosowany_kraj == e.target.feature.properties.NAME) {
+      openPopup_1("correct guess! :)")
+      punkty_text++
+      map.setView([36.38309138759306, 22.3209691411363], 3)
+      pokaz_europa(zrodlo, wskazana_liczba_kraji)
+    } else {
+      openPopup_1("you didn't guess it ://")
+      if (serca_text.length == 1) {
+        serca_text = serca_text.slice(0, -1)
+        openPopup_1("you lost! don't worry, you can try again")
+        serca_text = "❤❤❤❤❤❤"
+        punkty_text = 0
+      }
+      serca_text = serca_text.slice(0, -1)
+      map.setView([36.38309138759306, 22.3209691411363], 3)
+      pokaz_europa(zrodlo, wskazana_liczba_kraji)
+    }
+  }
+  //co sie stanie po wyjachaniu z kraju myszka
+  function resetHighlight(e) {
+    geojson.resetStyle(e.target)
+  }
+  // co sie stanie po najechaniu na kraj
+  function highlightFeature(e) {
+    var layer = e.target
+    //ustawia styl
+    layer.setStyle({
+      weight: 5,
+      color: "#666",
+      dashArray: "",
+      fillOpacity: 0.7,
+    })
+
+    layer.bringToFront()
+  }
+  //dopisanie funkcji do eventow
+  function onEachFeature(feature, layer) {
+    layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+      click: zoomToFeature,
+    })
+  }
+  //usuwanie starej warstwy geojsona
+  map.eachLayer(function (layer) {
+    if (layer instanceof L.Polygon) layer.remove()
+  })
+  //dodawnie do mapy
+  geojson = L.geoJson(zrodlo, {
+    style: { color: "#757678" },
+    onEachFeature: onEachFeature,
+  }).addTo(map)
+
+  //! losowanie liczb
+  var aaa = []
+  for (var i = 0; i <= wskazana_liczba_kraji; i++) {
+    aaa.push(i)
+  }
+
+  function* shuffle(tablica) {
+    var i = tablica.length
+
+    while (i--) {
+      yield tablica.splice(Math.floor(Math.random() * (i + 1)), 1)[0]
+    }
+  }
+  var ranNums = shuffle(aaa)
+  //! odświeżanie mapy, wczytywanie liczby serc, wszytywanie liczby punktow, pokazanie mapy ukrycie ekranu startowego
   main.style.visibility = "visible"
   document.getElementById("menu").style.visibility = "hidden"
-
-  wylosowany_kraj = kraje.features[ranNums.next().value].properties.NAME
-
   pytanie.innerHTML = ""
 
-  const h1 = document.createElement("h1")
-  h1.innerHTML = `country: ${wylosowany_kraj}`
-  h1.classList.add("wylosowany_kraj")
-  pytanie.appendChild(h1)
+  wylosowany_kraj = zrodlo.features[ranNums.next().value].properties.NAME
 
-  const serca_h1 = document.createElement("h1")
-  serca_h1.innerHTML = `${serca_text}`
-  serca_h1.classList.add("serca")
-  pytanie.appendChild(serca_h1)
+  console.log()
+  //budowanie elementow pokazujacych informacje
+  var h1
+  zbuduj_element(h1, "h1", `country: ${wylosowany_kraj}`, "wylosowany_kraj")
 
-  const punkty = document.createElement("h1")
-  punkty.innerHTML = `correct: ${punkty_text}`
-  punkty.classList.add("punkty")
-  pytanie.appendChild(punkty)
+  var serca_h1
+  zbuduj_element(serca_h1, "h1", `${serca_text}`, "serca")
+
+  var punkty
+  zbuduj_element(punkty, "h1", `correct: ${punkty_text}`, "punkty")
+}
+function zbuduj_element(name, typ, text, klasa) {
+  name = document.createElement(typ)
+  name.innerHTML = text
+  name.classList.add(klasa)
+  pytanie.appendChild(name)
+}
+//popup okienko
+function openPopup_1(err_msg) {
+  popup_1.classList.add("open-popup_1")
+  body.style.overflowY = "hidden"
+  err.innerHTML = err_msg
+}
+function closePopup_1() {
+  console.log("href")
+  popup_1.classList.remove("open-popup_1")
+  body.style.overflowY = "auto"
 }
