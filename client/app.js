965cc2b6a395f916c0fe96d87c01f1ab5046ac7f
antost360 <antost360@gmail.com>
2023-06-03 12:15:18 +0200

    finished for now

--- client/app.js
+++ client/app.js
@@ -1,20 +1,20 @@
-//!glowne zmienne
-//?kordy standardowego widoku mapy dla kazdego trybu gry, uzyte linijka 30 i 166 funckja wybor
+//!globalne zmienne ↓↓↓
+//kordy standardowego widoku mapy dla kazdego trybu gry, uzyte linijka 30 i 166 funckja wybor
 const europa_view = [52.964686207970026, 21.94101311132203],
   all_view = [35.43811453375265, 21.054874361484423],
   states_view = [41.077618404624815, -98.43108795964298],
   wojewodztwa_view = [52.39303121429481, 20.38909401093015]
-var wylosowany_kraj,
-  serca_text = "❤❤❤❤❤",
-  punkty_text = 0
 const main = document.getElementById("main"),
   menu = document.getElementById("menu"),
   pytanie = document.getElementById("pytanie"),
   err = document.getElementById("err"),
   popup_1 = document.getElementById("popup_1_id")
-
-//! początkowe ustawianie mapy, okienka, źródło mapy(1 link)
-//deklaracja mapy i deklaracja wyskakujacego okienka
+var wylosowany_kraj,
+  serca_text = "❤❤❤❤❤",
+  punkty_text = 0
+//!globalne zmienne ↑↑↑
+//! deklaracje map, zrodel, ↓↓↓
+//mapa do zgadywania
 var map = L.map("map", { minZoom: 3, zoomSnap: 0.25 }).setView([36.38309138759306, 22.3209691411363], 3),
   popup = L.popup()
 //zrodlo mapy
@@ -23,8 +23,7 @@ L.tileLayer("http://a.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png", {
   className: "mapa",
 }).addTo(map)
 
-//! druga mapa do odp poprawnych
-//deklaracja mapy i deklaracja wyskakujacego okienka
+//mapa do odpowiedzi
 var map2 = L.map("map2", { minZoom: 3, zoomSnap: 0.25 }).setView([36.38309138759306, 22.3209691411363], 4),
   popup2 = L.popup()
 //zrodlo mapy
@@ -32,52 +31,55 @@ L.tileLayer("http://a.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png", {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   className: "mapa",
 }).addTo(map2)
-
-// function pokaz_pop_odp(zrodlo, kraj_cel) {
-//   document.getElementById("map").style.display = "none"
-//   pytanie.style.display = "none"
-//   document.getElementById("map2").style.display = "block"
-
-//   for (let i = 0; i <= zrodlo.features.length - 1; i++) {
-//     var kraj = zrodlo.features[i]
-
-//     if (kraj.properties.NAME == kraj_cel) {
-//       var mapkraj = L.geoJSON(kraj, { color: "green" }).addTo(map2)
-//     } else {
-//       var mapkraj = L.geoJSON(kraj, { color: "whitesmoke" }).addTo(map2)
-//     }
-//   }
-// }
-
-//! wczytywanie liczby serc, punktow, pokazanie mapy, stats i  ukrycie ekranu startowego
-
+//! deklaracje map, zrodel,  ↑↑↑
+//! globalnie uzywane funkcje ↓↓↓
+function ustaw_widok(zrodlo, mapa) {
+  var dlugosc = zrodlo.features.length
+  if (dlugosc == 51) mapa.setView(europa_view, 3.8)
+  else if (dlugosc == 255) mapa.setView(all_view, 3)
+  else if (dlugosc == 50) mapa.setView(states_view, 4)
+  else if (dlugosc == 16) mapa.setView(wojewodztwa_view, 6)
+}
 function* shuffle(tablica) {
   var i = tablica.length
   while (i--) {
     yield tablica.splice(Math.floor(Math.random() * (i + 1)), 1)[0]
   }
 }
-
+// wybor trybu gry, ustawienie zrodla geojson i liczby kraji/stanow
+function wybor(wybor) {
+  if (wybor == "europa") {
+    pokaz_europa(europa, 44)
+    map.setView(europa_view, 3)
+  } else if (wybor == "all") {
+    pokaz_europa(kraje_all, 195)
+    map.setView(all_view, 3)
+  } else if (wybor == "states") {
+    pokaz_europa(states, 50)
+    map.setView(states_view, 3)
+  } else if (wybor == "wojewodztwa") {
+    pokaz_europa(wojewodztwa, 16)
+    map.setView(wojewodztwa_view, 6)
+  }
+}
+function back_to_start() {
+  aaa = []
+  close_game_over_okno()
+  window.location.reload()
+}
+//! globalnie uzywane funkcje ↑↑↑
+//!glowna funkcja ↓↓↓
 var aaa = []
 var ranNums = shuffle(aaa)
 
 function pokaz_europa(zrodlo, wskazana_liczba_kraji) {
   close_game_over_okno()
-  // close_okno_easter_egg()
-  document.getElementById("map").style.display = "block"
-  pytanie.style.display = "flex"
-  document.getElementById("map2").style.display = "none"
 
   main.style.visibility = "visible"
   menu.style.visibility = "hidden"
   pytanie.innerHTML = ""
 
-  //ustawienie widoku pod kazdy game mode, kazdy ma inna dlugosc tak je rozpoznuje
-  var dlugosc = zrodlo.features.length
-  if (dlugosc == 51) map.setView(europa_view, 3.8)
-  else if (dlugosc == 255) map.setView(all_view, 3)
-  else if (dlugosc == 50) map.setView(states_view, 4)
-  else if (dlugosc == 16) map.setView(wojewodztwa_view, 6)
+  ustaw_widok(zrodlo, map)
 
   //! losowanie liczb
 
@@ -85,13 +87,12 @@ function pokaz_europa(zrodlo, wskazana_liczba_kraji) {
 
   var index = ranNums.next()
 
-  if (index.done != true) {
-    wylosowany_kraj = zrodlo.features[index.value].properties.NAME
-  } else {
+  if (index.done != true) wylosowany_kraj = zrodlo.features[index.value].properties.NAME
+  else {
     close_alert_okno()
     open_game_over_okno(`You lost on ${wylosowany_kraj}`, `🏆final score🏆 ${punkty_text}`, zrodlo)
   }
-  //! budowanie elementow DOM
+  //! budowanie elementow DOM, div pytanie
   function zbuduj_element(name, typ, text, klasa, onclick) {
     name = document.createElement(typ)
     name.classList.add(klasa)
@@ -115,7 +116,7 @@ function pokaz_europa(zrodlo, wskazana_liczba_kraji) {
   function zoomToFeature(e) {
     //poprawna odpowiedz
     if (wylosowany_kraj == e.target.feature.properties.NAME) {
-      open_alert_okno("✅correct guess!✅")
+      open_alert_okno("correct guess!")
       punkty_text++
       if (punkty_text == 16) {
         open_game_over_okno(`Congrats, You completed whole game!`, `🏆final score🏆 ${punkty_text}`, zrodlo)
@@ -131,9 +132,9 @@ function pokaz_europa(zrodlo, wskazana_liczba_kraji) {
         return
       }
       //niepoprawna odp
-      open_alert_okno("❌you didn't guess it❌")
+      //?open_alert_okno("❌you didn't guess it❌")
+      pokaz_odp_okno(zrodlo, wylosowany_kraj)
       serca_text = serca_text.slice(0, -1)
-      // pokaz_pop_odp(zrodlo, wylosowany_kraj)
       pokaz_europa(zrodlo, wskazana_liczba_kraji)
     }
   }
@@ -172,22 +173,24 @@ function pokaz_europa(zrodlo, wskazana_liczba_kraji) {
     onEachFeature: onEachFeature,
   }).addTo(map)
 }
+//!glowna funkcja ↑↑↑
 
-//!popup okienko
-//? kod do "alert_okno"
+//! okno po poprawnej odpowiedzi ↓↓↓
 const okno = document.getElementById("alert_okno"),
   inside = document.getElementById("inside_alert_okno").style
 function open_alert_okno(err_msg) {
   //show modal pokazuje alert na stronie
   okno.showModal()
   err.innerHTML = err_msg
-  //kolorowanie ramki alertu okno
-  err_msg == "✅correct guess!✅" ? (inside.borderColor = "lightgreen") : (inside.borderColor = "red")
+  err.style.color = "lightGreen"
+  inside.borderColor = "lightgreen"
 }
 function close_alert_okno() {
   okno.close()
 }
-//? kod do "game_over_okno"
+//! okno po poprawnej odpowiedzi ↑↑↑
+
+//! okno do GAME OVER ↓↓↓
 const game_over_okno = document.getElementById("game_over_okno"),
   country = document.getElementById("country_game_over"),
   score = document.getElementById("score_game_over")
@@ -209,30 +212,39 @@ function open_game_over_okno(err_msg, score_int, baza) {
 function close_game_over_okno() {
   game_over_okno.close()
 }
-//! wybor trybu gry, ustawienie zrodla geojson i liczby kraji/stanow
-function wybor(wybor) {
-  if (wybor == "europa") {
-    pokaz_europa(europa, 44)
-    map.setView(europa_view, 3)
-  } else if (wybor == "all") {
-    pokaz_europa(kraje_all, 195)
-    map.setView(all_view, 3)
-  } else if (wybor == "states") {
-    pokaz_europa(states, 50)
-    map.setView(states_view, 3)
-  } else if (wybor == "wojewodztwa") {
-    pokaz_europa(wojewodztwa, 16)
-    map.setView(wojewodztwa_view, 6)
+//! okno do GAME OVER ↑↑↑
+
+//! okno okno po niepoprawnej odpowiedzi ↓↓↓
+const name_odp = document.getElementById("name_odp"),
+  odp_okno = document.getElementById("odp_okno"),
+  inside_odp_okno = document.getElementById("inside_odp_okno")
+var geojson_warstwa
+function pokaz_odp_okno(zrodlo, kraj_cel) {
+  inside_odp_okno.style.borderColor = "red"
+
+  if (geojson_warstwa != undefined) map2.removeLayer(geojson_warstwa)
+
+  //dodanie stylu do kraji, zielony = poprawna odp reszta standard
+  function style(feature) {
+    if (feature.properties.NAME === kraj_cel) {
+      return {
+        color: "green",
+      }
+    } else {
+      return {
+        color: "whitesmoke",
+      }
+    }
   }
+
+  geojson_warstwa = L.geoJson(zrodlo, { style: style }).addTo(map2)
+
+  ustaw_widok(zrodlo, map2)
+
+  name_odp.innerHTML = `correct answer: ${kraj_cel}`
+  odp_okno.showModal()
 }
-//! wroc do menu funckja
-function back_to_start() {
-  aaa = []
-  close_game_over_okno()
-  window.location.reload()
-  // main.style.visibility = "hidden"
-  // menu.style.visibility = "visible"
-  // pytanie.innerHTML = ""
-  // serca_text = "❤❤❤❤❤"
-  // punkty_text = 0
+function close_okp_okno() {
+  odp_okno.close()
 }
+//! okno okno po niepoprawnej odpowiedzi ↑↑↑
